1c28459efe30a1142eec7a690b87f5254f128aa9
Ankit <ankitchakraborty03@gmail.com>
2023-09-19 02:02:43 +0530

    Updates

---
+++ precache-manifest.c70a62e77d4e1d310a869e2c4a0c43dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cb80b4613fa6029d208",
+    "url": "/AICTE/static/css/main.5dd05eea.chunk.css"
+  },
+  {
+    "revision": "5cb80b4613fa6029d208",
+    "url": "/AICTE/static/js/main.5cb80b46.chunk.js"
+  },
+  {
+    "revision": "42feb57688907137dc06",
+    "url": "/AICTE/static/js/1.42feb576.chunk.js"
+  },
+  {
+    "revision": "ba226fc5c3a10c6a273d",
+    "url": "/AICTE/static/js/runtime~main.ba226fc5.js"
+  },
+  {
+    "revision": "a4cf1bf031426db50063cd8a09acbe4a",
+    "url": "/AICTE/static/media/img-2.a4cf1bf0.jpg"
+  },
+  {
+    "revision": "1e2865cba9ac0e9a528350c789c1166f",
+    "url": "/AICTE/static/media/img-8.1e2865cb.webp"
+  },
+  {
+    "revision": "23501cbdd6bf0949d22028a2a84e63bf",
+    "url": "/AICTE/index.html"
+  }
+];
