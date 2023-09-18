681e61e07dd9384e0c3fb17150cc1ed080076fc4
Ankit <ankitchakraborty03@gmail.com>
2023-09-19 01:32:28 +0530

    Login page added

---
+++ build/precache-manifest.633ba6ad3186da26613e90d575f6bbce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d091948b723f344337c8",
+    "url": "/AICTE/static/css/main.12045f63.chunk.css"
+  },
+  {
+    "revision": "d091948b723f344337c8",
+    "url": "/AICTE/static/js/main.d091948b.chunk.js"
+  },
+  {
+    "revision": "42feb57688907137dc06",
+    "url": "/AICTE/static/js/1.42feb576.chunk.js"
+  },
+  {
+    "revision": "ba226fc5c3a10c6a273d",
+    "url": "/AICTE/static/js/runtime~main.ba226fc5.js"
+  },
+  {
+    "revision": "a424326c5613cccb8f3c63b1b4bc3dbb",
+    "url": "/AICTE/index.html"
+  }
+];
